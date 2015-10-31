20676e42abb8c08b940b251f4f2922c2148e95c0
Petar Ivanov <pepincho95@gmail.com>
2015-10-31 19:38:13 +0200

    add 02_challenge_spec, tests

--- Ruby/02_challenge/02_challenge_spec.rb
+++ Ruby/02_challenge/02_challenge_spec.rb
@@ -2,5 +2,7 @@ describe 'button_presses' do
   it 'counts the number of pressed buttons' do
     expect(button_presses('nvm')).to eq 6
     expect(button_presses('WHERE DO U WANT 2 MEET L8R')).to eq 47
+    expect(button_presses(' 19*YrS 00#Ho5')).to eq 33
+    expect(button_presses('1234567890 *#')).to eq 40
   end
 end
