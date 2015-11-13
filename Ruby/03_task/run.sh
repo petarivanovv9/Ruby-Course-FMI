4b215a71e28a56d47a75b5594fcd06d960949903
Petar Ivanov <pepincho95@gmail.com>
2015-11-13 16:29:39 +0200

    fixed

--- Ruby/03_task/run.sh
+++ Ruby/03_task/run.sh
@@ -1,6 +1,6 @@
 echo "Running skeptic..."
-skeptic --lines-per-method 15 --line-length 100 --max-nesting-depth 2 --methods-per-class 10 --no-semicolons --no-trailing-whitespace --check-syntax --no-global-variables --english-words-for-names='fibonacci' --naming-conventions 03_task.rb
+skeptic --lines-per-method 15 --line-length 100 --max-nesting-depth 2 --methods-per-class 10 --no-semicolons --no-trailing-whitespace --check-syntax --no-global-variables --english-words-for-names='fibonacci' --naming-conventions my_solution.rb
 printf "\n"
 
 echo "Running spec..."
-rspec spec.rb --require ./03_task.rb --colour --format documentation
+rspec spec.rb --require ./my_solution.rb --colour --format documentation
